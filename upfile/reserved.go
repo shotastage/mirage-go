aae9429b613cb4af9380e1589fc80cb527ad05b8
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-22 22:14:00 +0900

    revise

---
+++ upfile/reserved.go
@@ -0,0 +1,12 @@
+package upfile
+
+const IMPORT = "introduce"
+const IF = "if"
+const FOR = "for"
+const FUNCTION = "@"
+const FUNCTION_2 = "fx"
+const CLASS = "class"
+const PUBLIC_METHOD = "+"
+const PRIVATE_METHOD = "-"
+const BEGIN_STATEMENT = "{"
+const END_STATEMENT = "}"
