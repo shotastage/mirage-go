6a59e77e7ef886a42665fb1df1b90eb66c17c1e9
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-22 15:45:07 +0900

    revise

--- ios/ios.go
+++ ios/ios.go
@@ -1,5 +1,15 @@
 package ios
 
+import "os/exec"
+
+func command(cmd string) error {
+	out, err := exec.Command("ls", "-la").Output()
+	println(out)
+
+	return err
+}
+
 func Bootstrap() {
 	println("Bootstrapping iOS... is now under construction")
+	command("pod init")
 }
