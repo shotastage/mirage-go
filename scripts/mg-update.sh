72045db5ea81466e00fa9eae3dadb4f7ed6d2500
Shota Shimazu <hornet.live.mf@gmail.com>
2019-08-17 17:51:13 +0900

    update

--- scripts/mg-update.sh
+++ scripts/mg-update.sh
@@ -1,3 +1,45 @@
 #!/usr/bin/env bash
 
-echo "MIRAGE Go Update is now under construction."
+
+function install_body() {
+    mkdir .mirage-tool/
+    cd .mirage-tool/
+    echo "Downloading update..."
+    git clone https://github.com/shotastage/mirage-go.git >> dev/null
+    cd mirage-go/
+    echo "Building..."
+    go build 
+    mv mirage-go mg
+    cd ..
+
+    mkdir bin
+    mv ./mirage-go/mg ./bin/mg 
+
+    mv ./mirage-go/scripts/mg-update.sh ./bin/
+    cd ./bin/
+    mv mg-update.sh mg-update
+}
+
+
+
+function restore_config() {
+    rm -rf .mirage-tool-tmp/
+    echo "Restore config:: NOW UNDER CONSTRUCTION"
+}
+
+
+function restore_data() {
+    echo "Restore data:: NOW UNDER CONSTRUCTION"
+}
+
+# HOME DIRECTORY
+cd
+
+# Backup files
+mv .mirage-tool/ .mirage-tool-tmp/
+
+rm -rf .mirage-tool/
+
+install_body
+restore_config
+restore_data
