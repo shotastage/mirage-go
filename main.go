bfde8dbcfae7569cb289822fa3618aaf4c7aed5e
Shota Shimazu <hornet.live.mf@gmail.com>
2019-08-18 14:42:30 +0900

    add platform check

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"mirage-go/mgpkg"
+	"mirage-go/os"
 	"mirage-go/touch"
 )
 
@@ -12,9 +14,18 @@ func main() {
 
 	if args[0] == "bs" || args[0] == "bootstrap" {
 		mgpkg.Bootstrap()
+		return
 	}
 
 	if args[0] == "cr" || args[0] == "create" {
 		touch.Touch(args[1])
+		return
 	}
+
+	if args[0] == "chk-pltfm" || args[0] == "check-platform" {
+		os.CheckPlatform()
+		return
+	}
+
+	fmt.Println("Given aurguments is invalid.")
 }
---
+++ os/os.go
@@ -0,0 +1,26 @@
+package os
+
+import (
+	"runtime"
+)
+
+func CheckPlatform() {
+	print("Your platform is ")
+
+	switch runtime.GOOS {
+	case "windows":
+		print("Windows")
+	case "darwin":
+		print("macOS")
+	case "linux":
+		print("Linux")
+	case "freebsd":
+		print("FreeBSD")
+	default:
+		print("Other OS")
+	}
+
+	print(".\n")
+
+	return
+}
