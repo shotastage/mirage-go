7a9e6f6f8fb5597e5a780dec4cfaa56f7beff95c
Shota Shimazu <hornet.live.mf@gmail.com>
2019-08-24 14:45:42 +0900

    complete uninstall scripts

--- scripts/mg-uninstall.sh
+++ scripts/mg-uninstall.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/env bash
 
+
+function uninstall() {
+    # rm -rf .mirage-tool/
+    echo "MIRAGE Go has been uninstalled!"
+    echo "Please remove PATH manually."
+}
+
+function yesno {
+    while true; do
+        echo "Are you sure to uninstall MIRAGE Go?"
+        echo "This operation can not be recovered."
+        echo -n "$* Please answer yes or no [y/n]: "
+        read ANS
+        case $ANS in
+        [Yy]*)
+            uninstall
+            ;;  
+        [Nn]*)
+            echo "Canceled"
+            exit 1
+            ;;
+        *)
+            echo "Please answer Y or N."
+            ;;
+    esac
+  done
+}
+
 cd
-
-rm -rf .mirage-tool/
-
-echo "MIRAGE Go has been uninstalled!"
-echo "Please remove PATH manually."
+yesno
